0d4f2ab8d697efd9b5f6357d7f14328c9863798d
davidcastle94 <46728183+davidcastle94@users.noreply.github.com>
2020-09-07 00:41:32 -0600

    Cliente,procuct. Bill class in progress we need to learn how to use data structures

---
+++ JavaApplication7/src/Model/Bill.java
@@ -0,0 +1,12 @@
+package Model;
+
+import Model.Customer;
+import Model.Article;
+import Model.Product;
+import Model.Service;
+
+public class Bill {
+    public double number_bill;
+    public Customer client;
+    public Product products;// this has to be a array 
+}
